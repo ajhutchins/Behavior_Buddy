05574314d579b814e0a27ab88816f9e10d1bf041
A.J. Hutchins <a.j.hutchins@Kates-MacBook-Air.local>
2021-04-12 10:31:40 -0400

    potentially last commit before presentation

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const APP = express()
 const PORT = process.env.PORT || 3003
 const MONGODB_URI = process.env.MONGODB_URI ||'mongodb://localhost:27017/' + 'behavior_buddy'
 
-
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose...')
@@ -32,7 +31,6 @@ APP.use(cors())
 APP.use(express.json())
 
 APP.use('/behaviors', behaviorsController)
-
 
 APP.listen(PORT, () => {
   console.log('🎉🎊', 'behavioral support happening on port', PORT, '🎉🎊',)
